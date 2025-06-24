e153aa3ba69ae89a984078d75dfe3e30c9ad520b
MatteoEmmerichRomiri <matteoemmerich@romiri.it>
2025-06-24 16:41:32 +0200

    AGG FUNZIONALITA'
    Aggiornamento Sviluppo

---
+++ DocumentoDiMagazzino.vb
@@ -0,0 +1,8 @@
+﻿Public Class DocumentoDiMagazzino
+    Public Property tipork As String
+    Public Property anno As Integer
+    Public Property serie As String
+    Public Property numdoc As Integer
+
+End Class
+
---
+++ Extensions/StringBuilderExtensions.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Text
+
+Public Module StringBuilderExtensions
+
+#Region "METODI APPEND LINE"
+
+    <System.Runtime.CompilerServices.Extension()>
+    Public Sub AppendLineFormat(ByRef sb As StringBuilder, format As String, ParamArray args() As Object)
+        Try
+
+            sb.AppendLine(String.Format(format, args) & " ")
+
+        Catch ex As Exception
+            Dim strErr As String = $"Errore generato nel modulo {NameOf(StringBuilderExtensions)} " &
+                         $"nel Metodo {NameOf(AppendLineFormat)}. " &
+                         $"Errore: " & ex.Message
+            Throw New Exception(strErr)
+        End Try
+
+    End Sub
+
+#End Region
+
+End Module
+
